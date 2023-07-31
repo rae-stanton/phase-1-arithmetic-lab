91cb52a043fd23881f58553bf47c501769e8cbb4
Rae Stanton <raestanton93@gmail.com>
2023-07-31 10:53:53 -0700

    math lab
    math lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const numbers = [1, 7, 12, 20];
+const max = Math.max(...numbers);
